e8723e5c29dc4c04f4985bf3ed6bfd6d3c9371aa
Rebecca Robison <rebeccahrobison@outlook.com>
2024-01-31 11:22:48 -0600

    Added barcode scanner files and capability to Add Repertoire view

--- client/src/components/repertoires/AddRepertoire.js
+++ client/src/components/repertoires/AddRepertoire.js
@@ -1,14 +1,67 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button, Form, FormGroup, Input, Label } from "reactstrap"
 import { addRepertoire } from "../../managers/repertoireManager"
 import { useNavigate } from "react-router-dom"
+import { getGoogleBookData } from "../../managers/barcodeServices"
+import { BarcodeScanner } from "../scanner/BarcodeScanner"
 
 export const AddRepertoire = () => {
   const [titleInput, setTitleInput] = useState("")
   const [authorInput, setAuthorInput] = useState("")
   const [imageInput, setImageInput] = useState("")
+  const [barcode, setBarcode] = useState(0)
+  const [googleBook, setGoogleBook] = useState([])
+  const [showScanner, setShowScanner] = useState(false)
 
   const navigate = useNavigate()
+
+  useEffect(() => {
+    return () => {
+      // Cleanup function to close the scanner when component unmounts
+      setShowScanner(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (barcode) {
+      getGoogleBookData(barcode).then(data => {
+        if(data?.items && data.items.length > 0) {
+          setGoogleBook(data?.items[0])
+        } else {
+          console.error("No items were found in the Google Book data.")
+        }
+      })
+    }
+  }, [barcode])
+
+  useEffect(() => {
+    if (googleBook) {
+      try {
+        const volumeInfo = googleBook.volumeInfo || {}
+        const authors = volumeInfo.authors || []
+        const categories = volumeInfo.categories || []
+        const imageLinks = volumeInfo.imageLinks || {}
+  
+        if (!volumeInfo.title || authors.length === 0 || categories.length === 0 || !imageLinks.thumbnail) {
+          throw new Error('Incomplete Google Book data: Missing required fields')
+        }
+        
+        setTitleInput(googleBook?.volumeInfo?.title)
+        setAuthorInput(googleBook?.volumeInfo?.authors[0])
+        setImageInput(googleBook?.volumeInfo?.imageLinks?.thumbnail)
+  
+      } catch (error) {
+        console.error(error)
+      }
+
+      }
+  }, [googleBook])
+
+
+  const handleUseBarcodeScannerBtn = () => {
+    setShowScanner(!showScanner)
+  }
+
 
   const handleAddRepertoireBtn = (e) => {
     e.preventDefault()
@@ -59,6 +112,12 @@ export const AddRepertoire = () => {
         </Form>
         <img className="add-image-cover" src={imageInput} />
       </div>
+      {showScanner && <BarcodeScanner setBarcode={setBarcode} showScanner={showScanner} />}
+      {showScanner ?
+          <Button className="barcode-scanner-btn" variant="secondary" onClick={handleUseBarcodeScannerBtn}>Close Barcode Scanner</Button>
+          :
+          <Button className="barcode-scanner-btn" variant="secondary" onClick={handleUseBarcodeScannerBtn}>Use Barcode Scanner</Button>
+        }
       <Button color="primary" onClick={handleAddRepertoireBtn}>Add Repertoire</Button>
     </div>
   )
---
+++ client/src/managers/barcodeServices.js
@@ -0,0 +1,15 @@
+export const getGoogleBookData = (barcode) => {
+  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${barcode}&key=${process.env["REACT_APP_APIKEY"]}`)
+    .then(async (res) => {
+    //  res.json())
+      if (!res.ok) {
+        throw new Error(`Failed to fetch data from Google Books API. Status: ${res.status}`)
+      }
+      const data = await res.json()
+      return data
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error)
+      return null
+    })
+}
